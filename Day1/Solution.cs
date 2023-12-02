3d29714e3028422c0b0f43f724b4a4fb48de2172
Sion Lloyd <sionlloyd101@hotmail.com>
2023-12-02 22:33:04 +0000

    Solve Day 1

--- Day1/Solution.cs
+++ Day1/Solution.cs
@@ -1,12 +1,65 @@
-﻿namespace Day1;
+using System.Globalization;
+using System.Net.Http.Headers;
+using Microsoft.VisualBasic;
 
- public class Solution : ISolution
+namespace Day1;
+
+public class Solution : ISolution
+{
+    string[] numbers = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+
+    public void Run()
     {
+        var data = File.ReadAllText("Day1\\Input1.txt").Split(Environment.NewLine);
 
-
-        
-          public void Run()
+        var values = new List<int>();
+        foreach (var line in data)
         {
-             var data = File.ReadAllText("Day1\\Input1.txt");
+            int? first = null;
+            int? last = null;
+            for (int i = 0; i < line.Length; i++)
+            {
+                var number = GetNumber(line, i);
+                if (number != null)
+                {
+                    if (first == null)
+                    {
+                        first = number;
+                        last = number;
+                    }
+                    else
+                    {
+                        last = number;
+                    }
+                }
+            }
+            values.Add(int.Parse($"{first}{last}"));
         }
+
+        OutputBuilder builder = new();
+        builder.AppendLines(values.Select(x => x.ToString()));
+        builder.AppendLine("Calibration Document");
+        builder.AppendLine(values.Sum().ToString());
+
+        File.WriteAllText("Day1\\Output.txt", builder.ToString());
     }
+
+    private int? GetNumber(string line, int i)
+    {
+        if (char.IsDigit(line[i]))
+        {
+            return int.Parse(line[i].ToString());
+        }
+
+        var check = line.Substring(i);
+        for (int n = 0; n < numbers.Length; n++)
+        {
+            if (check.StartsWith(numbers[n]))
+            {
+                return n + 1;
+            }
+        }
+
+        return null;
+    }
+}
