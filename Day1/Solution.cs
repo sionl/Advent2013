7dad1149b8e7d2d07c17b7a65ee97b070335153b
Sion Lloyd <sionlloyd101@hotmail.com>
2023-12-02 21:39:41 +0000

    Add solution mapper

---
+++ Day1/Solution.cs
@@ -0,0 +1,12 @@
+﻿namespace Day1;
+
+ public class Solution : ISolution
+    {
+
+
+        
+          public void Run()
+        {
+             var data = File.ReadAllText("Day1\\Input1.txt");
+        }
+    }
---
+++ Utils/OutputBuilder.cs
@@ -0,0 +1,48 @@
+using System.Text;
+
+public class OutputBuilder
+{
+    private StringBuilder builder = new();
+
+    public OutputBuilder Append(string line)
+    {
+        builder.Append(line);
+        return this;
+    }
+
+    public OutputBuilder AppendLine(string line)
+    {
+        builder.AppendLine(line);
+        return this;
+    }
+
+    public OutputBuilder AppendNewLine()
+    {
+        builder.Append(Environment.NewLine);
+        return this;
+    }
+
+    public OutputBuilder AppendSpaces(int number)
+    {
+        for (int i = 0; i < number; i++)
+        {
+            builder.Append(" ");
+        }
+        return this;
+    }
+
+    public OutputBuilder AppendLines(IEnumerable<string> lines)
+    {
+        foreach (var line in lines)
+        {
+            builder.AppendLine(line);
+        }
+        builder.Append(Environment.NewLine);
+        return this;
+    }
+
+    public override string ToString()
+    {
+        return builder.ToString();
+    }
+}
